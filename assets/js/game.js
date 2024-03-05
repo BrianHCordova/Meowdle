05237e782d03ae10ebd547bd2e956a03471f035d
Kyle <kyleyee522@gmail.com>
2024-03-05 14:20:29 -0800

    added breeds list to local storage

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,4 +1,5 @@
-let catBreeds = [];
+// let catBreeds = [];
+let day = 68;
 
 function shuffle(array) {
 	let currentIndex = array.length;
@@ -13,19 +14,46 @@ function shuffle(array) {
 			array[currentIndex],
 		];
 	}
-
-	return array;
+	// localStorage.removeItem('cats');
+	updateBreedToLocal(array);
+	// return array;
 }
 
-fetch(`https://api.thecatapi.com/v1/breeds`)
-	.then(function (response) {
-		return response.json();
-	})
-	.then(function (data) {
-		// console.log(data);
-		for (let i = 0; i < data.length; i++) {
-			catBreeds.push(data[i].name);
+function updateBreedToLocal(catBreeds) {
+	localStorage.setItem('cats', JSON.stringify(catBreeds));
+}
+
+function updateDayToLocal() {
+	localStorage.setItem('day', day);
+}
+
+function fetchBreeds() {
+	// If the day exceeds 67 days, then we remove the previous breeds array from local storage then fetch a new randomized list, else we just grab the pre-existing cat breeds list from local storage
+	if (day > 67) {
+		localStorage.removeItem('cats');
+		catBreeds = JSON.parse(localStorage.getItem('cats'));
+
+		if (catBreeds === null) {
+			catBreeds = [];
 		}
-		shuffle(catBreeds);
-		// console.log(catBreeds);
-	});
+
+		// Fetches API on page load
+		fetch(`https://api.thecatapi.com/v1/breeds`)
+			.then(function (response) {
+				return response.json();
+			})
+			.then(function (data) {
+				// console.log(data);
+
+				for (let i = 0; i < data.length; i++) {
+					catBreeds.push(data[i].name);
+				}
+				shuffle(catBreeds);
+				console.log(catBreeds);
+			});
+	} else {
+		catBreeds = JSON.parse(localStorage.getItem('cats'));
+	}
+}
+
+fetchBreeds();
