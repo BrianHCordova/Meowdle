38d39bd9155b5c77ddb6fb2a350ad2963b49ad9c
Kyle <kyleyee522@gmail.com>
2024-03-05 12:55:24 -0800

    randomized cat breeds order

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,5 +1,22 @@
 let catBreeds = [];
 
+function shuffle(array) {
+	let currentIndex = array.length;
+	let randomIndex;
+
+	while (currentIndex > 0) {
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex--;
+
+		[array[currentIndex], array[randomIndex]] = [
+			array[randomIndex],
+			array[currentIndex],
+		];
+	}
+
+	return array;
+}
+
 fetch(`https://api.thecatapi.com/v1/breeds`)
 	.then(function (response) {
 		return response.json();
@@ -9,4 +26,6 @@ fetch(`https://api.thecatapi.com/v1/breeds`)
 		for (let i = 0; i < data.length; i++) {
 			catBreeds.push(data[i].name);
 		}
+		shuffle(catBreeds);
+		// console.log(catBreeds);
 	});
