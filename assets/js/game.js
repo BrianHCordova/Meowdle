1026233f74178445266ffbab3ac8facab9e4d9ff
Kyle <kyleyee522@gmail.com>
2024-03-06 14:47:21 -0800

    added the reset date to local storage

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,5 +1,25 @@
 // let catBreeds = [];
-let day = 68;
+// let day = 68;
+// let sequence = [];
+const sequence1 = [
+	59, 51, 35, 53, 37, 44, 34, 36, 17, 11, 33, 26, 7, 49, 1, 12, 2, 10, 22, 32,
+	66, 16, 0, 61, 15, 5, 30, 19, 18, 43, 48, 29, 50, 60, 65, 63, 21, 45, 57, 3,
+	38, 27, 46, 55, 54, 9, 62, 52, 23, 24, 8, 6, 40, 25, 13, 31, 56, 39, 28, 41,
+	14, 4, 20, 58, 42, 47, 64,
+];
+
+// console.log(sequence1[0]);
+// const sequence2 = [3, 0, 27, 6, 61, 48, 43, 14, 47, 42, 11, 24, 46, 28, 26, 23, 15, 51, 2, 33, 30, 52, 17, 66, 53, 12, 16, 21, 25, 10, 13, 36, 54, 58, 8, 60, 64, 4, 44, 57, 1, 41, 49, 5, 50, 59, 18, 39, 32, 37, 65, 9, 7, 29, 38, 45, 56, 19, 20, 63, 55, 22, 62, 31, 35, 34, 40]
+//const sequence3 = [52, 53, 60, 15, 64, 11, 1, 14, 32, 43, 21, 61, 47, 50, 4, 40, 20, 54, 29, 66, 39, 36, 0, 16, 37, 3, 10, 13, 51, 24, 63, 62, 9, 18, 30, 56, 19, 5, 26, 31, 34, 57, 2, 6, 55, 27, 59, 35, 25, 33, 46, 12, 44, 28, 22, 42, 17, 8, 65, 23, 45, 49, 38, 7, 48, 58, 41]
+
+// function getSequence() {
+// 	for (let i = 0; i < 67; i++) {
+// 		sequence.push(i);
+// 	}
+// }
+
+// HARD CODE A START DATE (this will act as a starting point)
+// THEN FROM THERE WE CAN UPDATE LOCAL STORAGE TO KEEP UP WITH THE RESETS
 
 function shuffle(array) {
 	let currentIndex = array.length;
@@ -15,45 +35,116 @@ function shuffle(array) {
 		];
 	}
 	// localStorage.removeItem('cats');
-	updateBreedToLocal(array);
-	// return array;
+	// updateBreedToLocal(array);
+	return array;
 }
 
-function updateBreedToLocal(catBreeds) {
-	localStorage.setItem('cats', JSON.stringify(catBreeds));
-}
+// function updateBreedToLocal(catBreeds) {
+// 	localStorage.setItem('cats', JSON.stringify(catBreeds));
+// }
 
-function updateDayToLocal() {
-	localStorage.setItem('day', day);
-}
+// function updateDayToLocal() {
+// 	localStorage.setItem('day', day);
+// }
 
 function fetchBreeds() {
 	// If the day exceeds 67 days, then we remove the previous breeds array from local storage then fetch a new randomized list, else we just grab the pre-existing cat breeds list from local storage
-	if (day > 67) {
-		localStorage.removeItem('cats');
-		catBreeds = JSON.parse(localStorage.getItem('cats'));
 
-		if (catBreeds === null) {
-			catBreeds = [];
-		}
+	// localStorage.removeItem('cats');
+	// catBreeds = JSON.parse(localStorage.getItem('cats'));
 
-		// Fetches API on page load
-		fetch(`https://api.thecatapi.com/v1/breeds`)
-			.then(function (response) {
-				return response.json();
-			})
-			.then(function (data) {
-				// console.log(data);
+	// if (catBreeds === null) {
+	// 	catBreeds = [];
+	// }
 
-				for (let i = 0; i < data.length; i++) {
-					catBreeds.push(data[i].name);
-				}
-				shuffle(catBreeds);
-				console.log(catBreeds);
-			});
-	} else {
-		catBreeds = JSON.parse(localStorage.getItem('cats'));
-	}
+	// Fetches API
+	fetch(`https://api.thecatapi.com/v1/breeds`)
+		.then(function (response) {
+			return response.json();
+		})
+		.then(function (data) {
+			// console.log(data);
+			// for (let i = 0; i < data.length; i++) {
+			// 	catBreeds.push(data[i].name);
+			// }
+			// shuffle(catBreeds);
+			// console.log(catBreeds);
+		});
+
+	// catBreeds = JSON.parse(localStorage.getItem('cats'));
 }
 
-fetchBreeds();
+function addDays(date, period) {
+	//Creates copy of date
+	const dateCopy = new Date(date);
+	let totalDays = 0;
+
+	// While the difference of days exceeds 67, loop
+	while (period > 66) {
+		period = period - 67;
+		// Keep adding the dates by 68 so that the reset day happens when index goes back to 0
+		totalDays = totalDays + 67;
+	}
+
+	// After the difference between days is < 67 add the total # of days passed by the totalDays
+	dateCopy.setDate(date.getDate() + totalDays);
+
+	// Get the index
+	let index = period;
+	return [dateCopy, index];
+}
+
+function dateDiffInDays(a, b) {
+	const _MS_PER_DAY = 1000 * 60 * 60 * 24;
+	// Discard the time and time-zone information.
+	const utc1 = Date.UTC(a.getFullYear(), a.getMonth(), a.getDate());
+	const utc2 = Date.UTC(b.getFullYear(), b.getMonth(), b.getDate());
+
+	return Math.floor((utc2 - utc1) / _MS_PER_DAY);
+}
+
+// When user reopens the page, see if the date has passed at all
+function checkDate() {
+	// Obtain date from local storage if possible
+	// If there is no date grab the hard coded date (the VERY beginning date)
+	// Update it from there
+	let lastReset = new Date('1/1/2024');
+
+	const todaysDate = new Date();
+
+	const resetDate = JSON.parse(localStorage.getItem('date'));
+
+	if (resetDate) {
+		// If there is something inside the date local storage, then we should calculate the days to see if there has been a reset and the index
+		// If there has been a reset update the local storage
+		lastReset = new Date(resetDate);
+
+		if (lastReset.toISOString() === todaysDate.toISOString()) {
+			const index = 0;
+			const newDate = todaysDate;
+		} else {
+			const daysDifference = dateDiffInDays(lastReset, todaysDate);
+
+			const dateAndIndex = addDays(lastReset, daysDifference);
+			const newDate = dateAndIndex[0];
+			const index = dateAndIndex[1];
+
+			// Sets the new date when there is a reset
+			localStorage.setItem('date', JSON.stringify(newDate));
+			// localStorage.setItem('day', index);
+		}
+	} // Else if there is nothing in local storage, make the beginning date the last reset, then update the days to calculate which rotation we are on and the index
+	else {
+		const daysDifference = dateDiffInDays(lastReset, todaysDate);
+		const dateAndIndex = addDays(lastReset, daysDifference);
+
+		const newDate = dateAndIndex[0];
+		const index = dateAndIndex[1];
+
+		localStorage.setItem('date', JSON.stringify(newDate));
+		// console.log(`THIS IS WHEN LOCAL IS EMPTY (DATE): ${newDate}`);
+		// console.log(`THIS IS WHEN LOCAL IS EMPTY (INDEX): ${index}`);
+	}
+}
+checkDate();
+// If it has, update the now current date and calculate the difference in days so that we can traverse the array to match the right answer
